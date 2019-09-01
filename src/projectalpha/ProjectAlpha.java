02b0a2ca5c52ab0e76e2a41c455a19cb376a525a
Medadusik <sweez@DESKTOP-2FMLL9F>
2019-09-01 18:48:11 +0200

    Jav.App Ver 1.1

--- src/projectalpha/ProjectAlpha.java
+++ src/projectalpha/ProjectAlpha.java
@@ -1,18 +1,10 @@
-//Bulbosaurus
-//ahoj hovado hfhfh
 package projectalpha;
-public class ProjectAlpha {
+
+    public class ProjectAlpha {
+        
     public static void main(String[] args) {
-   System.out.println("HELLO WORLD"); 
-   System.out.println("HELLO WORLD");
-   System.out.println("HELLO WORLD");
-   System.out.println("HELLO WORLD");
-   System.out.println("HELLO WORLD");
-   System.out.println("HELLO WORLD");
-   System.out.println("HELLO WORLD");
-   System.out.println("HELLO WORLD");
-   System.out.println("ahoj");
-   //Test funkce push
+
+
    
     }
     
